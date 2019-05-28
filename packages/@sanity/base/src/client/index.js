f75ecf215fd8f26d0c10c80543358db52f514801
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-05-28 18:16:09 +0200

    [base] Deprecate studio client without a specified apiVersion

--- packages/@sanity/base/src/client/index.js
+++ packages/@sanity/base/src/client/index.js
@@ -1,29 +1,76 @@
+import generateHelpUrl from '@sanity/generate-help-url'
 import sanityClient from '@sanity/client'
 import config from 'config:sanity'
 import configureClient from 'part:@sanity/base/configure-client?'
 
-const deprecationMessage = `[deprecation] The Sanity client is now exposed in CommonJS format.
-
-For instance, change:
-  \`const client = require('part:@sanity/base/client').default\`
-
-To the following:
-  \`const client = require('part:@sanity/base/client')\`
-`
-
-const apiConfig = {...config.api, withCredentials: true, useCdn: false}
+const apiConfig = {...config.api, withCredentials: true, useCdn: false, apiVersion: '1'}
 const client = sanityClient(apiConfig)
 
 const configuredClient = configureClient ? configureClient(sanityClient(apiConfig)) : client
 
-// Warn when people use `.default`
-Object.defineProperty(configuredClient, 'default', {
-  get() {
-    // eslint-disable-next-line no-console
-    console.warn(deprecationMessage)
-    return configuredClient
+const getKeys = obj => Object.keys(Object.getPrototypeOf(obj)).concat(Object.keys(obj))
+const instances = [configuredClient]
+
+const wrappedClient = {
+  config(newConfig, silence = false) {
+    if (!newConfig) {
+      return configuredClient.config()
+    }
+
+    if (!silence) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        new Error(
+          `Setting configuration on the global studio client is deprecated - please use \`withConfig()\` instead - see ${generateHelpUrl(
+            'studio-client-global-config'
+          )}`
+        )
+      )
+    }
+
+    // Don't allow overriding apiVersion on instantiated clients
+    const {apiVersion, ...rest} = newConfig
+    instances.forEach(instance => instance.config(rest))
+    return wrappedClient
+  },
+
+  withConfig: newConfig => {
+    if (!newConfig || !newConfig.apiVersion) {
+      throw new Error(
+        `Client \`withConfig()\` called without an \`apiVersion\` - see ${generateHelpUrl(
+          'studio-client-specify-api-version'
+        )}`
+      )
+    }
+
+    const newClient = configuredClient.clone().config(newConfig)
+    instances.push(newClient)
+    return newClient
   }
+}
+
+getKeys(configuredClient).forEach(key => {
+  if (key === 'config') {
+    return
+  }
+
+  Object.defineProperty(wrappedClient, key, {
+    get() {
+      // eslint-disable-next-line no-console
+      console.warn(
+        // Using Error to get a stack that makes it easier to see where it originates from
+        new Error(
+          `Used property "${key}" on versionless client - this is deprecated. Please specify API version using \`withConfig\` - see ${generateHelpUrl(
+            'studio-client-specify-api-version'
+          )}`
+        )
+      )
+
+      return configuredClient[key]
+    }
+  })
 })
 
 // Expose as CJS to allow Node scripts to consume it without `.default`
-module.exports = configuredClient
+// eslint-disable-next-line import/no-commonjs
+module.exports = wrappedClient
