6298dddcfeca5648bdaab770bac55440f1722325
vicbergquist <victoria.bergquist@gmail.com>
2019-11-04 13:04:27 +0100

    simplify and refactor file for previews

--- packages/test-studio/src/resolveContextualPreviews.js
+++ packages/test-studio/src/resolveContextualPreviews.js
@@ -3,52 +3,32 @@ import React from 'react'
 import ColorblindPreview from './components/previews/a11y/colorblind-filter/ColorblindPreview'
 import SeoPreviews from './components/previews/seo/SeoPreviews'
 
-const PREVIEW_TYPES = ['book', 'author']
-const PREVIEWS = [
-  {
-    name: 'colorblind',
-    title: 'Color blindness',
-    component: props => <ColorblindPreview {...props} />,
-    options: {
-      url: 'https://css-tricks.com'
+export default function resolveContextualPreviews(document) {
+  return [
+    {
+      name: 'colorblind',
+      title: 'Color blindness',
+      component: <ColorblindPreview url="https://css-tricks.com" />
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+      baseUrl: 'https://some-seo-url.no',
+      component: <SeoPreviews document={document} />
+    },
+    {
+      name: 'example-com',
+      title: 'Example.com',
+      url: 'https://example.com'
+    },
+    {
+      name: 'author-name',
+      title: 'Author Name',
+      component: (
+        <div>
+          <h1>{document.name}</h1>
+        </div>
+      )
     }
-  },
-  {
-    name: 'seo',
-    title: 'SEO',
-    baseUrl: 'https://some-seo-url.no',
-    component: props => <SeoPreviews {...props} />
-  },
-  {
-    name: 'example-com',
-    title: 'Example.com',
-    baseUrl: 'https://example.com'
-  },
-  {
-    name: 'author-name',
-    title: 'Author Name',
-    component: props => (
-      <div>
-        <h1>{props.document.name}</h1>
-      </div>
-    )
-  }
-]
-
-export default function resolveContextualPreviews(document, rev) {
-  if (!document) {
-    console.warn('No document to resolve preview from')
-    return []
-  }
-  if (!PREVIEW_TYPES.includes(document._type)) {
-    console.warn('Document is of unwanted _type. If you want to preview, that is.')
-    return []
-  }
-  return PREVIEWS.map(item => {
-    if (item.baseUrl) {
-      const url = `${item.baseUrl}/${document._id}`
-      return {...item, url: rev ? `${url}?rev=${rev}` : url}
-    }
-    return item
-  })
+  ]
 }
