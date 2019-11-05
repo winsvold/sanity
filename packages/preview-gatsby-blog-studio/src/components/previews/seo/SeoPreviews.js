bd50eafa92a1f96d68fbc20dcca2f6a79e3e04ca
Thomas Drevon <thomas@sanity.io>
2019-11-05 13:40:18 +0100

    Add FB preview and move doc materializing

--- packages/preview-gatsby-blog-studio/src/components/previews/seo/SeoPreviews.js
+++ packages/preview-gatsby-blog-studio/src/components/previews/seo/SeoPreviews.js
@@ -1,9 +1,18 @@
 /* eslint-disable react/no-multi-comp, react/no-did-mount-set-state, react/forbid-prop-types */
 import React from 'react'
 import PropTypes from 'prop-types'
+import sanityClient from 'part:@sanity/base/client'
+import Spinner from 'part:@sanity/components/loading/spinner'
 import GoogleSearchResult from './GoogleSearchResult'
 import TwitterCard from './TwitterCard'
+import FacebookShare from './FacebookShare'
 import styles from './SeoPreviews.css'
+
+const materializeDocument = documentId => {
+  return sanityClient.fetch(`*[_id == $documentId][0]{..., "authors": authors[].author->}`, {
+    documentId
+  })
+}
 
 class SeoPreviews extends React.PureComponent {
   static propTypes = {
@@ -14,14 +23,35 @@ class SeoPreviews extends React.PureComponent {
     document: null
   }
 
+  state = {
+    materializedDocument: null
+  }
+
+  static async getDerivedStateFromProps(props, state) {
+    const newState = await materializeDocument(props.document._id).then(materializedDocument => ({
+      materializedDocument
+    }))
+    return newState
+  }
+
+  componentDidMount() {
+    materializeDocument(this.props.document._id).then(materializedDocument =>
+      this.setState({materializedDocument})
+    )
+  }
+
   render() {
-    const {document} = this.props
+    const document = this.state.materializedDocument
+    if (!document) {
+      return <Spinner />
+    }
     console.log('document', document)
 
     return (
       <div>
         <GoogleSearchResult document={document} />
         <TwitterCard document={document} />
+        <FacebookShare document={document} />
       </div>
     )
   }
--- packages/preview-gatsby-blog-studio/src/components/previews/seo/TwitterCard.js
+++ packages/preview-gatsby-blog-studio/src/components/previews/seo/TwitterCard.js
@@ -1,9 +1,8 @@
-/* eslint-disable react/no-multi-comp, react/no-did-mount-set-state, react/forbid-prop-types */
+/* eslint-disable react/no-unused-prop-types, react/no-multi-comp, react/no-did-mount-set-state, react/forbid-prop-types */
 import React from 'react'
 import PropTypes from 'prop-types'
 import imageUrlBuilder from '@sanity/image-url'
 import sanityClient from 'part:@sanity/base/client'
-import Spinner from 'part:@sanity/components/loading/spinner'
 import {assemblePostUrl, blocksToText, websiteUrl} from '../../../utils'
 import styles from './SeoPreviews.css'
 
@@ -11,12 +10,6 @@ const builder = imageUrlBuilder(sanityClient)
 
 const urlFor = source => {
   return builder.image(source)
-}
-
-const materializeDocument = documentId => {
-  return sanityClient.fetch(`*[_id == $documentId][0]{..., "authors": authors[].author->}`, {
-    documentId
-  })
 }
 
 const placeholderAuthor = {
@@ -33,29 +26,8 @@ class TwitterCard extends React.PureComponent {
     document: null
   }
 
-  state = {
-    materializedDocument: null
-  }
-
-  static async getDerivedStateFromProps(props, state) {
-    const newState = await materializeDocument(props.document._id).then(materializedDocument => ({
-      materializedDocument
-    }))
-    return newState
-  }
-
-  componentDidMount() {
-    materializeDocument(this.props.document._id).then(materializedDocument =>
-      this.setState({materializedDocument})
-    )
-  }
-
   render() {
-    const document = this.state.materializedDocument
-    if (!document) {
-      return <Spinner />
-    }
-    console.log('-------->', document)
+    const {document} = this.props
     const {title, body, mainImage} = document
     const url = assemblePostUrl(document)
     const primaryAuthor = {...placeholderAuthor, ...document.authors[0]}
