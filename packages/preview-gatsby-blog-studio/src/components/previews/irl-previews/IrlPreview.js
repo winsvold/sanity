2d932599e4611b8fbef0f6bd095fd95e633138a3
vicbergquist <victoria.bergquist@gmail.com>
2019-11-08 23:49:29 +0100

    add IRL previews

---
+++ packages/preview-gatsby-blog-studio/src/components/previews/irl-previews/IrlPreview.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Moveable from 'react-moveable'
+import RadioButtons from 'part:@sanity/components/selects/radio'
+import styles from './IrlPreview.css'
+
+const EDIT_MODES = [
+  {title: 'Resize', name: 'resizable'},
+  {title: 'Scale', name: 'scalable'},
+  {title: 'Warp', name: 'warpable'}
+]
+
+const IMG_FILTERS = [
+  {title: 'Hudson', name: 'hudson'},
+  {title: 'Brooklyn', name: 'brooklyn'},
+  {title: 'Amaro', name: 'amaro'},
+  {title: 'Gingham', name: 'gingham'}
+]
+
+class IrlPreview extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bannerTarget: null,
+      editMode: EDIT_MODES[0],
+      imgFilter: IMG_FILTERS[0],
+      isEditMode: false
+    }
+    this.previewBanner = React.createRef()
+    this.matrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+    this.scale = [1, 1]
+
+  }
+
+  static propTypes = {
+    position: PropTypes.string,
+    previewImage: PropTypes.string.isRequired,
+    document: PropTypes.object
+  }
+
+  static defaultProps = {
+    document: null,
+    position: ''
+  }
+
+  handleEditModeChange = item => {
+    this.setState({
+      editMode: item
+    })
+  }
+
+  handleImgFilterChange = item => {
+    this.setState({
+      imgFilter: item
+    })
+  }
+
+  handleShowEditMode = () => {
+    this.setState({
+      isEditMode: true
+    })
+  }
+
+  handleHideEditMode = () => {
+    this.setState({
+      isEditMode: false
+    })
+  }
+
+  componentDidMount() {
+    this.setState({
+      bannerTarget: this.previewBanner.current
+    })
+    this.previewBanner.current.setAttribute('style', this.props.position)
+  }
+
+  render() {
+    const {document, previewImage} = this.props
+    const {bannerTarget, editMode, isEditMode, imgFilter} = this.state
+    return (
+      <div className={styles.componentWrapper}>
+        <div className={styles.radioWrapper}>
+          <label>Filter</label>
+          <RadioButtons
+            value={imgFilter}
+            items={IMG_FILTERS}
+            onChange={this.handleImgFilterChange}
+          />
+        </div>
+        <div className={styles.radioWrapper}>
+          <label>Edit mode</label>
+          <RadioButtons value={editMode} items={EDIT_MODES} onChange={this.handleEditModeChange} />
+        </div>
+        <div
+          className={`${styles.previewWrapper} ${styles.filter} ${styles[imgFilter.name]}`}
+          onMouseEnter={this.handleShowEditMode}
+          onMouseLeave={this.handleHideEditMode}
+        >
+          <img className={styles.backgroundImage} src={previewImage} />
+          {bannerTarget && isEditMode && (
+            <Moveable
+              target={bannerTarget}
+              draggable
+              resizable={editMode.name === 'resizable'}
+              warpable={editMode.name === 'warpable'}
+              scalable={editMode.name === 'scalable'}
+              onWarp={({target, multiply, delta}) => {
+                this.matrix = multiply(this.matrix, delta)
+                target.style.transform = `scale(${this.scale[0]},${
+                  this.scale[1]
+                }) matrix3d(${this.matrix.join(',')})`
+              }}
+              onDrag={({target, left, top, beforeDelta}) => {
+                target.style.left = `${left}px`
+                target.style.top = `${top}px`
+              }}
+              onResize={({target, width, height, dist}) => {
+                target.style.width = `${width}px`
+                target.style.height = `${height}px`
+              }}
+              onScale={({target, delta}) => {
+                const scale = this.scale
+                scale[0] *= delta[0]
+                scale[1] *= delta[1]
+                target.style.transform = `scale(${scale[0]}, ${
+                  scale[1]
+                }) matrix3d(${this.matrix.join(',')})`
+              }}
+            />
+          )}
+          <div ref={this.previewBanner} className={styles.banner}>
+            {document.title}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IrlPreview
--- packages/preview-gatsby-blog-studio/src/resolveContextualPreviews.js
+++ packages/preview-gatsby-blog-studio/src/resolveContextualPreviews.js
@@ -4,6 +4,7 @@ import ColorblindPreview from './components/previews/a11y/colorblind-filter/Colo
 import TextToSpeechPreview from './components/previews/a11y/text-to-speech/TextToSpeechPreview'
 import SeoPreviews from './components/previews/seo/SeoPreviews'
 import {assemblePostUrl} from './utils'
+import IrlPreview from './components/previews/irl-previews/IrlPreview'
 
 const resolveContextualPreviews = document => {
   const isPost = document._type === 'post'
@@ -12,16 +13,41 @@ const resolveContextualPreviews = document => {
   return [
     isPost
       ? {
-          name: 'text-to-speech',
-          title: 'Text-to-Speech',
+          name: 'billboard-newyork',
+          title: 'Times Square',
           component: (
-            <TextToSpeechPreview
-              document={document}
-              options={{fields: ['title', 'body', 'excerpt'], rate: 0.5, pitch: 1}}
-            />
+            <IrlPreview position="background: rgb(36, 87, 114); height: 156px; width: 467px;left: 86px; top: 172px; transform: scale(1.0059, 1.01289) matrix3d(0.16931, -0.265774, 0, -0.00165839, -0.0339945, 0.636538, 0, -0.000462379, 0, 0, 1, 0, 141.494, -12.404, 0, 1);" document={document} previewImage="/static/newyork2.jpg" />
           )
         }
       : null,
+    isPost
+      ? {
+          name: 'billboard-osaka',
+          title: 'Osaka',
+          component: <IrlPreview position="background: rgb(36, 87, 114); width: 187px; height: 384px; left: 285px; top: 25px; font-size: 1.3rem; color: black; display: flex; align-items: center; text-align: center; font-weight: bold; transform: scale(1, 1) matrix3d(0.684027, 0.101256, 0, 0.000730198, 0.010567, 0.835693, 0, -0.000350746, 0, 0, 1, 0, -28.4262, -23.8987, 0, 1);" document={document} previewImage="/static/osaka.png" />
+        }
+      : null,
+    isPost
+      ? {
+          name: 'billboard-newyork-2',
+          title: 'New York 2',
+          component: (
+            <IrlPreview position="background: rgb(36, 87, 114); width: 392px; height: 238px; left: 47px; top: 112px; font-size: 1.3rem; color: white; display: flex; align-items: center; text-align: center; font-weight: bold; transform: scale(1, 1) matrix3d(0.786713, 0.180528, 0, 0.000450564, -0.0499065, 0.730603, 0, -0.000410337, 0, 0, 1, 0, 80.4607, -30.8489, 0, 1);" document={document} previewImage="/static/newyork.png" />
+          )
+        }
+      : null,
+    // isPost
+    //   ? {
+    //       name: 'text-to-speech',
+    //       title: 'Text-to-Speech',
+    //       component: (
+    //         <TextToSpeechPreview
+    //           document={document}
+    //           options={{fields: ['title', 'body', 'excerpt'], rate: 1.3, pitch: 1, lang: 'nb-NO'}}
+    //         />
+    //       )
+    //     }
+    //   : null,
     isPost
       ? {
           name: 'colorblind',
@@ -29,29 +55,18 @@ const resolveContextualPreviews = document => {
           component: <ColorblindPreview url={assemblePostUrl(document)} />
         }
       : null,
-    isPost
-      ? {
-          name: 'web-frontend',
-          title: 'Web frontend',
-          url: `${assemblePostUrl(document)}`
-        }
-      : null,
+    // isPost
+    //   ? {
+    //       name: 'website',
+    //       title: 'Website',
+    //       url: `${assemblePostUrl(document)}`
+    //     }
+    //   : null,
     isPost
       ? {
           name: 'seo',
           title: 'SEO',
           component: <SeoPreviews document={document} />
-        }
-      : null,
-    isAuthor
-      ? {
-          name: 'author-name',
-          title: 'Author Name',
-          component: (
-            <div>
-              <h1 style={{background: 'linear-gradient(#e66465, #9198e5)'}}>{document.name}</h1>
-            </div>
-          )
         }
       : null
   ].filter(Boolean)
