909561cad3f5b3dbacc265b80b94dc1fe2e41685
Thomas Drevon <thomas@sanity.io>
2019-11-11 10:45:13 +0100

    [preview-nextjs-landing-pages-studio] Primitive fetching of logo url and business card

--- packages/preview-nextjs-landing-pages-studio/src/components/previews/pdf/BusinessCard.js
+++ packages/preview-nextjs-landing-pages-studio/src/components/previews/pdf/BusinessCard.js
@@ -2,12 +2,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Spinner from 'part:@sanity/components/loading/spinner'
+import sanityClient from 'part:@sanity/base/client'
+import imageUrlBuilder from '@sanity/image-url'
 import getIt from 'get-it'
 import promise from 'get-it/lib/middleware/promise'
 import styles from './BusinessCard.css'
 
-const cardGeneratorUrl = 'http://localhost:3000/api/business-card?fileType=png'
+const fileType = 'png'
+const cardServiceBaseUrl = `http://localhost:3000/api/business-card?fileType=${fileType}`
 const request = getIt([promise()])
+
+const builder = imageUrlBuilder(sanityClient)
+
+const urlFor = source => {
+  return builder.image(source)
+}
 
 class BusinessCard extends React.PureComponent {
   static propTypes = {
@@ -23,23 +32,30 @@ class BusinessCard extends React.PureComponent {
     error: null
   }
 
-  // TODO: materialize profileImageURL?
-  // alternatively, use sanityClient on the server thingy to get the image url
   componentDidMount = () => {
     const {document} = this.props
-    const url = `${cardGeneratorUrl}&document=${JSON.stringify(document)}`
-    console.log('Fetching business card from', url)
-    request({url, rawBody: true})
-      .then(response => {
-        const base64 = btoa(
-          new Uint8Array(response.body).reduce((data, byte) => data + String.fromCharCode(byte), '')
-        )
-        this.setState({businessCardImage: `data:image/png;base64,${base64}`})
-      })
-      .catch(error => {
-        console.error('BOOOM', error)
-        this.setState({error})
-      })
+    sanityClient.fetch('*[_id == "global-config"][0]').then(siteConfig => {
+      const siteLogoImageUrl = urlFor(siteConfig.logo)
+        .width(500)
+        .url()
+      document.imageUrl = siteLogoImageUrl
+      const stringifiedDoc = JSON.stringify(document)
+      const cardServiceUrl = `${cardServiceBaseUrl}&document=${stringifiedDoc}&imageUrl=${siteLogoImageUrl}`
+
+      request({url: cardServiceUrl, rawBody: true})
+        .then(response => {
+          const base64 = btoa(
+            new Uint8Array(response.body).reduce(
+              (data, byte) => data + String.fromCharCode(byte),
+              ''
+            )
+          )
+          this.setState({businessCardImage: `data:image/${fileType};base64,${base64}`})
+        })
+        .catch(error => {
+          this.setState({error})
+        })
+    })
   }
 
   render() {
@@ -50,12 +66,14 @@ class BusinessCard extends React.PureComponent {
     if (error) {
       return <pre>{JSON.stringify(error, null, 2)}</pre>
     }
+    if (!businessCardImage) {
+      return <Spinner message="Fetching business card" />
+    }
 
     return (
       <div className={styles.root}>
-        <h3>BusinessCard for {name}</h3>
-        {!businessCardImage && <Spinner />}
-        {businessCardImage && <img className={styles.cardWrapper} src={businessCardImage} />}
+        <h3>{`Here's your business card, ${name}`}</h3>
+        <img className={styles.cardWrapper} src={businessCardImage} />
       </div>
     )
   }
