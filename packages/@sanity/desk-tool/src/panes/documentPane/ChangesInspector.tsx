df7582d10de56c9b3aed15a8736361c1b8930ee7
Marius Lundgård <studio@mariuslundgard.com>
2020-04-21 11:09:51 +0200

    [desk-tool] Begin ChangeInspector

---
+++ packages/@sanity/desk-tool/src/panes/documentPane/ChangesInspector.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import styles from './ChangesInspector.css'
+
+export default function ChangesInspector(): React.ReactElement {
+  return <div className={styles.root}>ChangesInspector</div>
+}
