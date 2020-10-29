28f0f8373b35fb6913d3739e7150113365d55c0e
Marius Lundgård <studio@mariuslundgard.com>
2020-10-29 13:47:19 +0100

    [base] Fix part definitions

--- packages/@sanity/base/src/@types/parts.d.ts
+++ packages/@sanity/base/src/@types/parts.d.ts
@@ -1,7 +1,7 @@
-/* eslint-disable import/no-duplicates */
+/* eslint-disable import/export, import/no-duplicates */
+
 // @todo: define interface
 declare module 'part:@sanity/base/authentication-fetcher'
-declare module 'part:@sanity/components/dialogs/fullscreen-message?'
 
 declare module 'config:sanity' {
   interface SanityConfig {
@@ -15,12 +15,23 @@ declare module 'config:sanity' {
   export default config
 }
 
+declare module 'sanity:css-custom-properties' {
+  const cssCustomProperties: Record<string, string | undefined>
+  export default cssCustomProperties
+}
+
 declare module 'sanity:versions' {
   type PackageName = string
   type VersionNumber = string
   const versions: Record<PackageName, VersionNumber>
   export default versions
 }
+
+declare module 'part:@sanity/base/close-icon'
+declare module 'part:@sanity/base/circle-check-icon'
+declare module 'part:@sanity/base/warning-icon'
+declare module 'part:@sanity/base/error-icon'
+declare module 'part:@sanity/base/info-icon'
 
 declare module 'part:@sanity/base/configure-client?' {
   import {SanityClient as OriginalSanityClient} from '@sanity/client'
@@ -42,6 +53,12 @@ declare module 'part:@sanity/base/client' {
 // @todo: convert to TS
 declare module 'part:@sanity/base/util/draft-utils'
 
+declare module 'part:@sanity/base/root' {
+  const RootComponent: React.ComponentType
+
+  export default RootComponent
+}
+
 declare module 'part:@sanity/base/schema' {
   // @todo: define Schema interface
   type Schema = any
@@ -53,6 +70,15 @@ declare module 'part:@sanity/base/schema' {
 
 declare module 'part:@sanity/components/avatar' {
   export * from '@sanity/components/src/avatar'
+}
+
+declare module 'part:@sanity/components/buttons/default' {
+  export * from '@sanity/components/src/buttons/DefaultButton'
+  export {default} from '@sanity/components/src/buttons/DefaultButton'
+}
+
+declare module 'part:@sanity/components/dialogs/fullscreen-message' {
+  export {default} from '@sanity/components/src/dialogs/FullscreenMessageDialog'
 }
 
 declare module 'part:@sanity/components/layer' {
@@ -73,6 +99,10 @@ declare module 'part:@sanity/components/popover' {
 
 declare module 'part:@sanity/components/scroll' {
   export * from '@sanity/components/src/scroll'
+}
+
+declare module 'part:@sanity/components/snackbar/provider' {
+  export {default} from '@sanity/components/src/snackbar/SnackbarProvider'
 }
 
 declare module 'all:part:*'
