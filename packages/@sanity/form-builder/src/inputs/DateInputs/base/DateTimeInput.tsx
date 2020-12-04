b58a7bb5208f93f3a781440176c8c6cf0b9fd6bf
Bjørge Næss <bjoerge@gmail.com>
2020-12-04 12:14:45 +0100

    [form-builder] Rewrite date inputs to use date picker based on UI
    Co-authored-by: Benedicte Emilie Brækken <benedicte@sanity.io>
    Co-authored-by: Marius Lundgård <studio@mariuslundgard.com>

---
+++ packages/@sanity/form-builder/src/inputs/DateInputs/base/DateTimeInput.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import FocusLock from 'react-focus-lock'
+
+import {Box, Button, Layer, Popover, useClickOutside, useForwardedRef} from '@sanity/ui'
+import {DatePicker} from './DatePicker'
+import {LazyTextInput} from './LazyTextInput'
+
+type Props = {
+  value?: Date
+  id?: string
+  readOnly?: boolean
+  selectTime?: boolean
+  timeStep?: number
+  customValidity?: string
+  placeholder?: string
+  onInputChange?: (event) => void
+  inputValue?: string
+  onChange: (date: Date | null) => void
+}
+
+export const DateTimeInput = React.forwardRef(function DateTimeInput(
+  props: Props,
+  forwardedRef: React.ForwardedRef<HTMLInputElement>
+) {
+  const {value, inputValue, onInputChange, onChange, selectTime, timeStep, ...rest} = props
+
+  const [popoverRef, setPopoverRef] = React.useState<HTMLElement | null>(null)
+
+  const inputRef = useForwardedRef(forwardedRef)
+  const buttonRef = React.useRef(null)
+
+  const [isPickerOpen, setPickerOpen] = React.useState(false)
+
+  useClickOutside(() => setPickerOpen(false), [popoverRef])
+
+  return (
+    <LazyTextInput
+      ref={inputRef}
+      {...rest}
+      value={inputValue}
+      onChange={onInputChange}
+      suffix={
+        <Layer depth={1000}>
+          <Popover
+            ref={setPopoverRef}
+            content={
+              <FocusLock
+                onDeactivation={() => {
+                  inputRef.current?.focus()
+                  inputRef.current?.select()
+                }}
+              >
+                <DatePicker
+                  selectTime={selectTime}
+                  timeStep={timeStep}
+                  onKeyUp={(e) => {
+                    if (e.key === 'Escape') {
+                      setPickerOpen(false)
+                    }
+                  }}
+                  value={value}
+                  onChange={onChange}
+                />
+              </FocusLock>
+            }
+            padding={1}
+            placement="bottom-end"
+            open={isPickerOpen}
+          >
+            <Box padding={1}>
+              <Button
+                ref={buttonRef}
+                icon="calendar"
+                mode="bleed"
+                padding={2}
+                onClick={() => setPickerOpen(true)}
+                style={{display: 'block'}}
+              />
+            </Box>
+          </Popover>
+        </Layer>
+      }
+    />
+  )
+})
