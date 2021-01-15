54cf445beb7d81a84bca614bf240dffc5995d7ab
Marius Lundgård <studio@mariuslundgard.com>
2021-01-15 11:59:10 +0100

    [form-builder] Convert InvalidValue to functional component

--- packages/@sanity/form-builder/src/inputs/arrays/PortableText/InvalidValue.tsx
+++ packages/@sanity/form-builder/src/inputs/arrays/PortableText/InvalidValue.tsx
@@ -1,49 +1,50 @@
 import {InvalidValueResolution, PortableTextBlock} from '@sanity/portable-text-editor'
 import {Button} from '@sanity/ui'
-import React from 'react'
+import React, {useCallback} from 'react'
 import {Warning} from '../../../components/Warning'
 import styles from '../../ObjectInput/styles/UnknownFields.css'
 
-type InvalidValueProps = {
-  resolution: InvalidValueResolution
-  value: PortableTextBlock[]
+interface InvalidValueProps {
   onChange: (...args: any[]) => any
   onIgnore: () => void
+  resolution: InvalidValueResolution
+  // eslint-disable-next-line react/no-unused-prop-types
+  value: PortableTextBlock[]
 }
 
-export default class InvalidValue extends React.PureComponent<InvalidValueProps> {
-  handleAction = (): void => {
-    const resolution = this.props.resolution
+export function InvalidValue(props: InvalidValueProps) {
+  const {onChange, onIgnore, resolution} = props
+
+  const handleAction = useCallback(() => {
     if (resolution) {
       const {patches} = resolution
-      this.props.onChange({type: 'mutation', patches})
+      onChange({type: 'mutation', patches})
     }
-  }
-  handleIgnore = (): void => {
-    this.props.onIgnore()
-  }
-  render() {
-    const {resolution} = this.props
-    const message = (
-      <>
-        <p>{resolution.description}</p>
-        <p>
-          <pre className={styles.inspectValue}>{JSON.stringify(resolution.item, null, 2)}</pre>
-        </p>
-        {resolution.action && (
-          <>
-            <div className={styles.buttonWrapper}>
-              <Button tone="primary" onClick={this.handleAction} text={resolution.action} />
-              <Button mode="ghost" onClick={this.handleIgnore} text="Ignore" />
-            </div>
-            <p>
-              It’s generally safe to perform the action above, but if you are in doubt, get in touch
-              with those responsible for configuring your studio.
-            </p>
-          </>
-        )}
-      </>
-    )
-    return <Warning heading="Invalid portable text value" message={message} />
-  }
+  }, [onChange, resolution])
+
+  const message = (
+    <>
+      <p>{resolution.description}</p>
+
+      <p>
+        <pre className={styles.inspectValue}>{JSON.stringify(resolution.item, null, 2)}</pre>
+      </p>
+
+      {resolution.action && (
+        <>
+          <div className={styles.buttonWrapper}>
+            <Button tone="primary" onClick={handleAction} text={resolution.action} />
+            <Button mode="ghost" onClick={onIgnore} text="Ignore" />
+          </div>
+
+          <p>
+            It’s generally safe to perform the action above, but if you are in doubt, get in touch
+            with those responsible for configuring your studio.
+          </p>
+        </>
+      )}
+    </>
+  )
+
+  return <Warning heading="Invalid portable text value" message={message} />
 }
