5c63c1145cc44147bc2a8e97a04f04c494ed77be
Marius Lundgård <studio@mariuslundgard.com>
2021-01-18 10:15:49 +0100

    [form-builder] Migrate Warning component to Sanity UI

--- packages/@sanity/form-builder/src/components/Warning.tsx
+++ packages/@sanity/form-builder/src/components/Warning.tsx
@@ -1,51 +1,38 @@
+import {ErrorOutlineIcon, WarningOutlineIcon} from '@sanity/icons'
+import {Box, Card, Flex, Stack, Text} from '@sanity/ui'
 import React from 'react'
 import {Details} from '../components/Details'
-import styles from './Warning.css'
 
-type Props = {
-  heading?: any
-  message?: any
-  values?: any[]
-  children?: any
+interface WarningProps {
+  children?: React.ReactNode
+  heading: React.ReactNode
+  message: React.ReactNode
+  tone?: 'caution' | 'critical'
 }
 
-export function Warning({heading, message, values = [], children}: Props) {
-  const len = values.length
+export function Warning({heading, message, children, tone = 'caution'}: WarningProps) {
   return (
-    <div className={styles.root}>
-      <h2 className={styles.heading}>
-        {heading ? (
-          heading
-        ) : (
-          <>
-            Found {len === 1 ? <>a</> : len} {len === 1 ? <>value</> : <>values</>} with{' '}
-            {len === 1 && <>an</>} unknown {len === 1 ? <>type</> : <>types</>}
-          </>
-        )}
-      </h2>
-      <div>
-        <Details>
-          <div>
-            {message ? (
-              message
-            ) : (
-              <>
-                These are not defined in the current schema as valid types for this array. This
-                could mean that the type has been removed, or that someone else has added it to
-                their own local schema that is not yet deployed.
-                {values.map((item) => {
-                  return (
-                    <div key={item}>
-                      <pre>{JSON.stringify(item)}</pre>
-                    </div>
-                  )
-                })}
-              </>
-            )}
-          </div>
-          <div>{children && children}</div>
-        </Details>
-      </div>
-    </div>
+    <Card padding={2} radius={2} tone={tone}>
+      <Flex padding={3}>
+        <Box>
+          <Text size={1}>
+            {tone === 'caution' && <WarningOutlineIcon />}
+            {tone === 'critical' && <ErrorOutlineIcon />}
+          </Text>
+        </Box>
+
+        <Box flex={1} marginLeft={2}>
+          <Stack space={3}>
+            <Text size={1} weight="semibold">
+              {heading}
+            </Text>
+
+            <Details>{message}</Details>
+          </Stack>
+        </Box>
+      </Flex>
+
+      {children}
+    </Card>
   )
 }
